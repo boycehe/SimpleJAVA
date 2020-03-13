e9d72e83e852d1d3dbc758db4a2a9c8772cf7e48
boyce <peilin.hpl@alibaba-inc.com>
2020-03-13 12:36:51 +0800

    demo finish

--- semantic.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "simple_java_foundation.h"
-
-void createClass(JavaParser *parser,Token *clsToken,Token *parentClasToken,JavaClassBody *clsBody);
-JavaClassBody *dressedClassBodyWithVars(JavaClassBody *classBody,ExprList*vars);
-JavaClassBody *dressedClassBodyWithFunc(JavaClassBody *classBody,JavaFunction*func);
-JavaFunction *addFunctionToClass(JavaParser *parser,JavaAccessType accessType,Token *returnType,Token *name,ExprList *parametreList,JavaSentenceList *sentenceList);
-void addExprToFunc(JavaFunction *func,JavaSentence *senctence);
